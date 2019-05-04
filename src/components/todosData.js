13da0be6a396d4e3376ff4ed17372eecb273e2f6
The-lady-developer <manvisinghwal123@gmail.com>
2019-05-04 17:56:05 +0530

    to-do App

---
+++ src/components/todosData.js
@@ -0,0 +1,37 @@
+import React from "react"
+
+const todosData = [
+ {
+     id:1,
+     text: "Take out the trash",
+     completed: true
+ },
+
+ {
+    id:2,
+    text: "Groccery shopping",
+    completed: false
+},
+
+{
+    id:3,
+    text: "Clean the table",
+    completed: false
+},
+
+{
+    id:4,
+    text: "Did the coding?",
+    completed: true
+},
+
+{
+    id:5,
+    text: "Catch up with friends?",
+    completed: false
+}
+
+
+]
+
+export default todosData 
